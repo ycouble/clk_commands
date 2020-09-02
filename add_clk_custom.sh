bed54e0ecb1a8f15a6b6088cadd281184968d79b
Yoann Couble <yoann.couble@cosmotech.com>
2020-09-02 18:30:56 +0200

    added install to setup script

--- add_clk_custom.sh
+++ add_clk_custom.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+pip3 install pipx
+pipx install click-project
+pipx upgrade click-project
 clk customcommands add-external-path $(pwd)/external
 clk customcommands add-python-path $(pwd)/python
